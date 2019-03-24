db451902d8ee34e21f9ffead482414639ae2c0e6
Romulo Auccapuclla <rauccapuclla@gmail.com>
2019-03-23 22:00:06 -0500

    fix build status

--- vars/slackMessage.groovy
+++ vars/slackMessage.groovy
@@ -2,7 +2,7 @@
 
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+  buildStatus =  buildStatus ?: 'SUCCESS'
 
   // Default values
   def colorName = 'RED'
@@ -16,7 +16,7 @@ def call(String buildStatus = 'STARTED') {
   if (buildStatus == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
+  } else if (buildStatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
   } else {
